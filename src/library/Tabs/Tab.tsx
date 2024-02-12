f7e29805293157d20d37b5975fc60854f01dfd8b
Ross Bulat <ross@parity.io>
2024-02-12 11:37:29 +0700

    add tab contextmenu event

--- src/library/Tabs/Tab.tsx
+++ src/library/Tabs/Tab.tsx
@@ -1,9 +1,26 @@
 // Copyright 2024 @rossbulat/console authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
+import { useRef } from 'react';
 import { TabWrapper } from './Wrappers';
 import type { TabProps } from './types';
+import { useEventListener } from 'usehooks-ts';
 
-export const Tab = ({ name, active }: TabProps) => (
-  <TabWrapper className={active ? 'active' : undefined}>{name}</TabWrapper>
-);
+export const Tab = ({ name, active }: TabProps) => {
+  useEventListener;
+
+  const tabRef = useRef<HTMLDivElement>(null);
+
+  // Handle context menu when tab is right clicked.
+  const handleTabContextMenu = (e: Event): void => {
+    e.preventDefault();
+  };
+
+  useEventListener('contextmenu', handleTabContextMenu, tabRef);
+
+  return (
+    <TabWrapper ref={tabRef} className={active ? 'active' : undefined}>
+      {name}
+    </TabWrapper>
+  );
+};
