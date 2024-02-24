7e544aeac1b7c4325bc9b1a4bb4f22156b064846
Ross Bulat <ross@parity.io>
2024-02-24 11:20:13 +0700

    new tag form boilerplate

--- src/screens/Settings/TagSettings/Wrappers.ts
+++ src/screens/Settings/TagSettings/Wrappers.ts
@@ -3,6 +3,39 @@
 
 import styled from 'styled-components';
 
+export const NewTagWrapper = styled.div`
+  border: 1px solid var(--border-primary-color);
+  background-color: var(--background-primary);
+  border-radius: 0.45rem;
+  padding: 0.75rem 0.85rem;
+  flex-grow: 1;
+  display: flex;
+  margin: 0.5rem 0;
+
+  > div {
+    display: flex;
+    align-items: flex-end;
+
+    &.input {
+      flex-grow: 1;
+      margin-bottom: 0.25rem;
+    }
+    &.controls {
+      flex-shrink: 1;
+      justify-content: flex-end;
+      align-items: center;
+
+      > button {
+        margin-left: 1.1rem;
+
+        &.cancel {
+          color: var(--text-color-tertiary);
+        }
+      }
+    }
+  }
+`;
+
 export const TagItemWrapper = styled.div`
   border-bottom: 1px solid var(--border-secondary-color);
   margin-bottom: 1rem;
--- src/screens/Settings/TagSettings/index.tsx
+++ src/screens/Settings/TagSettings/index.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 import { useTags } from 'contexts/Tags';
-import { TagItemWrapper } from './Wrappers';
+import { NewTagWrapper, TagItemWrapper } from './Wrappers';
 import { Tag } from 'library/Tag';
 import {
   HeaderButtonWrapper,
@@ -10,27 +10,60 @@ import {
   SettingsSubheadingWrapper,
 } from '../Wrappers';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLock, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faLock, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 export const TagSettings = () => {
   const { tags, getChainsForTag } = useTags();
   const totalTags = Object.keys(tags).length;
+
+  // Whether the new tag form is open.
+  const [newTagOpen, setNewTagOpen] = useState<boolean>(false);
+
+  // The current value of the new tag input.
+  const [newTagValue, setNewTagValue] = useState<string>('');
+
+  // Handler to cancel the new tag form.
+  const cancelNewTag = () => {
+    setNewTagOpen(false);
+    setNewTagValue('');
+  };
 
   return (
     <>
       <SettingsHeaderWrapper>
         <h2>Manage Tags</h2>
         <div>
-          <HeaderButtonWrapper
-            onClick={() => {
-              /* Do nothing */
-            }}
-          >
-            <FontAwesomeIcon icon={faPlus} transform="shrink-1" />
+          <HeaderButtonWrapper onClick={() => setNewTagOpen(!newTagOpen)}>
+            <FontAwesomeIcon
+              icon={newTagOpen ? faMinus : faPlus}
+              transform="shrink-1"
+            />
             New Tag
           </HeaderButtonWrapper>
         </div>
       </SettingsHeaderWrapper>
+
+      {newTagOpen && (
+        <NewTagWrapper>
+          <div className="input">
+            <input
+              type="text"
+              value={newTagValue}
+              placeholder="Tag Name"
+              onChange={(ev) => setNewTagValue(ev.target.value)}
+            />
+          </div>
+          <div className="controls">
+            <button className="cancel" onClick={() => cancelNewTag()}>
+              Cancel
+            </button>
+            <HeaderButtonWrapper onClick={() => setNewTagOpen(!newTagOpen)}>
+              Create Tag
+            </HeaderButtonWrapper>
+          </div>
+        </NewTagWrapper>
+      )}
 
       <SettingsSubheadingWrapper>
         {totalTags} {totalTags === 1 ? 'tag' : 'tags'}
