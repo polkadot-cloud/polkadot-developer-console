7031d89e4438f21e6e7acff8b75c8412607d2936
Ross Bulat <ross@parity.io>
2024-03-02 13:57:59 +0700

    active section styling

--- src/screens/Default/Menu.tsx
+++ src/screens/Default/Menu.tsx
@@ -8,7 +8,7 @@ import { useLocation } from 'react-router-dom';
 import { HeaderMenuWrapper, ButtonWrapper } from 'library/HeaderMenu/Wrappers';
 import type { PageProps } from 'screens/Utils';
 
-export const ChainMenu = ({ setSection }: PageProps) => {
+export const ChainMenu = ({ section, setSection }: PageProps) => {
   const { pathname } = useLocation();
   const { tabsHidden, setTabsHidden } = useTabs();
 
@@ -17,10 +17,18 @@ export const ChainMenu = ({ setSection }: PageProps) => {
       <div className="menu">
         <section className="main">
           <div className="label">Not Connected</div>
-          <button onClick={() => setSection(0)}>Chain</button>
+          <button
+            onClick={() => setSection(0)}
+            className={section === 0 ? 'active' : undefined}
+          >
+            Connect
+          </button>
         </section>
         <section className="other">
-          <button className="label" onClick={() => setSection(1)}>
+          <button
+            onClick={() => setSection(1)}
+            className={`label${section === 1 ? ` active` : ``}`}
+          >
             Manage Tab
           </button>
         </section>
