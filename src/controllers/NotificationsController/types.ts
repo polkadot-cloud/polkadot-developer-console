8ffdc57ab00da9f69fa6f819effbdb6552bfabe4
Ross Bulat <ross@parity.io>
2024-03-08 15:09:51 +0700

    fix licenses

--- src/controllers/NotificationsController/types.ts
+++ src/controllers/NotificationsController/types.ts
@@ -1,4 +1,4 @@
-// Copyright 2024 @paritytech/polkadot-staking-dashboard authors & contributors
+// Copyright 2024 @rossbulat/console authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
 export interface NotificationInterface {
