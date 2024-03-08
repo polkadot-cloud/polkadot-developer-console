99f4e57c32048e4fdfedb66d744fac1f4f8dcb25
Ross Bulat <ross@parity.io>
2024-03-08 15:11:29 +0700

    add licenses

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,5 @@
+// Copyright 2024 @rossbulat/console authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
