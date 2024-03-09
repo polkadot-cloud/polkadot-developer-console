0caa7861e690d45c0acfd4bb90be1b50f637bf1c
Ross Bulat <ross@parity.io>
2024-03-09 12:36:34 +0700

    init workspace

---
+++ src/screens/Settings/WorkspaceSettings/index.tsx
@@ -0,0 +1,10 @@
+// Copyright 2024 @rossbulat/console authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { SettingsHeaderWrapper } from 'library/Settings/Wrappers';
+
+export const WorkspaceSettings = () => (
+  <SettingsHeaderWrapper>
+    <h2>Workspace Settings</h2>
+  </SettingsHeaderWrapper>
+);
