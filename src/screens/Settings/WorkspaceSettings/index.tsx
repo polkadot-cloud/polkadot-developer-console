422aed1c1b131d60701ae7e877db40630f96ab66
Ross Bulat <ross@parity.io>
2024-03-09 12:52:56 +0700

    add in development

--- src/screens/Settings/WorkspaceSettings/index.tsx
+++ src/screens/Settings/WorkspaceSettings/index.tsx
@@ -12,61 +12,63 @@ import {
   faFileImport,
   faTriangleExclamation,
 } from '@fortawesome/free-solid-svg-icons';
+import { useMenu } from 'contexts/Menu';
+import { InDevelopment } from 'library/HelpMenu/InDevelopment';
 
-export const WorkspaceSettings = () => (
-  <>
-    <SettingsHeaderWrapper>
-      <h2>Workspace Settings</h2>
-    </SettingsHeaderWrapper>
+export const WorkspaceSettings = () => {
+  const { openMenu } = useMenu();
 
-    <SettingsToggleWrapper>
-      <div className="text">
-        <h4>Export Workspace</h4>
-        <h3>
-          Back up your current workspace state. Exports your tabs, tags, chain
-          search settings.
-        </h3>
-      </div>
-    </SettingsToggleWrapper>
+  return (
+    <>
+      <SettingsHeaderWrapper>
+        <h2>Workspace Settings</h2>
+      </SettingsHeaderWrapper>
 
-    <SettingsSubmitWrapper>
-      <div className="buttons">
-        <button
-          onClick={() => {
-            /* Do nothing */
-          }}
-        >
-          <FontAwesomeIcon icon={faDownload} />
-          Export Workspace
-        </button>
-      </div>
-    </SettingsSubmitWrapper>
+      <SettingsToggleWrapper>
+        <div className="text">
+          <h4>Export Workspace</h4>
+          <h3>
+            Back up your current workspace state. Exports your tabs, tags, chain
+            search settings.
+          </h3>
+        </div>
+      </SettingsToggleWrapper>
 
-    <SettingsToggleWrapper>
-      <div className="text">
-        <h4>Import Workspace</h4>
-        <h3>Import a workspace configuration.</h3>
-        <h3 className="inline danger">
-          <FontAwesomeIcon icon={faTriangleExclamation} />
-          &nbsp; Importing a workspace will replace your current workspace - all
-          current state, including your current tabs and custom tag settings,
-          will be lost. Back up your workspace first if you wish to restore it
-          later.
-        </h3>
-      </div>
-    </SettingsToggleWrapper>
+      <SettingsSubmitWrapper>
+        <div className="buttons">
+          <button
+            onClick={(ev) => openMenu(ev, <InDevelopment />, { size: 'large' })}
+          >
+            <FontAwesomeIcon icon={faDownload} />
+            Export Workspace
+          </button>
+        </div>
+      </SettingsSubmitWrapper>
 
-    <SettingsSubmitWrapper>
-      <div className="buttons">
-        <button
-          onClick={() => {
-            /* Do nothing */
-          }}
-        >
-          <FontAwesomeIcon icon={faFileImport} />
-          Import Workspace
-        </button>
-      </div>
-    </SettingsSubmitWrapper>
-  </>
-);
+      <SettingsToggleWrapper>
+        <div className="text">
+          <h4>Import Workspace</h4>
+          <h3>Import a workspace configuration.</h3>
+          <h3 className="inline danger">
+            <FontAwesomeIcon icon={faTriangleExclamation} />
+            &nbsp; Importing a workspace will replace your current workspace -
+            all current state, including your current tabs and custom tag
+            settings, will be lost. Back up your workspace first if you wish to
+            restore it later.
+          </h3>
+        </div>
+      </SettingsToggleWrapper>
+
+      <SettingsSubmitWrapper>
+        <div className="buttons">
+          <button
+            onClick={(ev) => openMenu(ev, <InDevelopment />, { size: 'large' })}
+          >
+            <FontAwesomeIcon icon={faFileImport} />
+            Import Workspace
+          </button>
+        </div>
+      </SettingsSubmitWrapper>
+    </>
+  );
+};
