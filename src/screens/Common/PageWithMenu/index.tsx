0ae03f0faa10da45d7bf04f6ead3b61869fff617
Ross Bulat <ross@parity.io>
2024-03-10 17:46:38 +0700

    override accent secondary

--- src/screens/Common/PageWithMenu/index.tsx
+++ src/screens/Common/PageWithMenu/index.tsx
@@ -5,15 +5,34 @@ import { Body } from 'library/Body';
 import { Page as PageWrapper } from 'library/Page';
 import { SectionProvider } from 'library/Page/provider';
 import type { PageWithMenuProps } from './types';
+import { NetworkDirectory, type DirectoryId } from 'config/networks';
+import { useTabs } from 'contexts/Tabs';
 
 // Renders a page and menu, with state controlling the active section of the page.
-export const PageWithMenu = ({ pageId, Page, Menu }: PageWithMenuProps) => (
-  <SectionProvider pageId={pageId}>
-    <Menu />
-    <Body>
-      <PageWrapper>
-        <Page />
-      </PageWrapper>
-    </Body>
-  </SectionProvider>
-);
+export const PageWithMenu = ({ pageId, Page, Menu }: PageWithMenuProps) => {
+  const { getActiveTab } = useTabs();
+  const tab = getActiveTab();
+
+  // Get colors from active chain id.
+  const chainId: DirectoryId | undefined =
+    (tab?.chain?.id as DirectoryId) || undefined;
+
+  // Whether to override the accent colors on this page.
+  const overrideCss = chainId && pageId !== 'settings';
+
+  // NOTE: hardcoding the developer console purple color as fallback for now.
+  const color = overrideCss ? NetworkDirectory[chainId]?.color : '#552bbf';
+
+  return (
+    <div style={Object.fromEntries([['--accent-color-secondary', color]])}>
+      <SectionProvider pageId={pageId}>
+        <Menu />
+        <Body>
+          <PageWrapper>
+            <Page />
+          </PageWrapper>
+        </Body>
+      </SectionProvider>
+    </div>
+  );
+};
