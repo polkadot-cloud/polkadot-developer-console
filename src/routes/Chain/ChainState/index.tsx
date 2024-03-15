e9917a7deb890707e08c21a907b8851cfa00385a
Ross Bulat <ross@parity.io>
2024-03-15 11:52:38 +0700

    init chain state selection markup

--- src/routes/Chain/ChainState/index.tsx
+++ src/routes/Chain/ChainState/index.tsx
@@ -1,9 +1,35 @@
 // Copyright 2024 @rossbulat/console authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { SelectChainStateWrapper, Wrapper } from './Wrapper';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
 export const ChainState = () => (
-  <>
-    <h2>Chain State</h2>
-    <h4>In development</h4>
-  </>
+  <Wrapper>
+    <SelectChainStateWrapper>
+      <section>
+        <h5>Pallet</h5>
+        <button className="input">
+          <span>
+            <h4>Pallet Name</h4>
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faChevronDown} transform="shrink-4" />
+          </span>
+        </button>
+      </section>
+      <section>
+        <h5>Storage Item</h5>
+        <button className="input">
+          <span>
+            <h4>Storage Item</h4>
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faChevronDown} transform="shrink-4" />
+          </span>
+        </button>
+      </section>
+    </SelectChainStateWrapper>
+  </Wrapper>
 );
