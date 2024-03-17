cc9630006405f470d1ed2e165c88d7e854988a91
Ross Bulat <ross@parity.io>
2024-03-17 11:41:53 +0700

    add v15 class

--- src/controllers/Metadata/index.ts
+++ src/controllers/Metadata/index.ts
@@ -5,11 +5,14 @@ import { MetadataV14 } from 'model/Metadata/MetadataV14';
 import type { MetadataVersion } from './types';
 import { UnknownMetadata } from 'model/Metadata/UnknownMetadata';
 import type { Metadata } from '@polkadot/types';
+import { MetadataV15 } from 'model/Metadata/MetadataV15';
 
 export class MetadataController {
   // Instantiate a metadata version.
   static instantiate(version: string, metadata: Metadata): MetadataVersion {
     switch (version) {
+      case 'v15':
+        return new MetadataV15(metadata);
       case 'v14':
         return new MetadataV14(metadata);
       default:
---
+++ src/model/Metadata/MetadataV15.ts
@@ -0,0 +1,19 @@
+// Copyright 2024 @rossbulat/console authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import type { Metadata } from '@polkadot/types';
+import type { MetadataVersion } from './types';
+import type { AnyJson } from '@w3ux/utils/types';
+
+export class MetadataV15 implements MetadataVersion {
+  metadata: Metadata;
+
+  constructor(metadata: Metadata) {
+    this.metadata = metadata;
+  }
+
+  getMetadataJson() {
+    const json: AnyJson = this.metadata.toJSON().metadata;
+    return json.v15;
+  }
+}
