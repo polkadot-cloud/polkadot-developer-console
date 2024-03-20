d45a7ede7e1a8c6f1f678e85dbfdd14510f30773
Ross Bulat <ross@parity.io>
2024-03-20 16:03:33 +0700

    create CallSignature class

---
+++ src/model/Metadata/Format/CallSignature.ts
@@ -0,0 +1,161 @@
+// Copyright 2024 @rossbulat/console authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import type { AnyJson } from '@w3ux/utils/types';
+
+export class FormatCallSignature {
+  // The raw input config to format.
+  #rawConfig: AnyJson;
+
+  constructor(rawConfig: AnyJson) {
+    this.#rawConfig = rawConfig;
+  }
+
+  // ------------------------------------------------------
+  // Call signature formatting.
+  // ------------------------------------------------------
+
+  // Formats `rawConfig`` data into a string.
+  format = () => {
+    const {
+      modifier,
+      type: { argTypes, returnType },
+    } = this.#rawConfig;
+
+    // Format arguments and return types.
+    const [argFormatted, returnFormatted] = [argTypes, returnType].map(
+      (section) => this.getTypeString(section)
+    );
+
+    // Format the call signature based on formatted types and modifier.
+    const callSig = `${this.formatArgType(argFormatted)}${this.formatReturnType(returnFormatted, modifier)}`;
+    return callSig;
+  };
+
+  // Formats call argument type.
+  formatArgType = (argType: string) => {
+    let str = '';
+    if (argType !== '') {
+      // Check if arguments are already wrapped in parens, and to do so if not.
+      if (!/^\(.*\)$/.test(argType)) {
+        str = `(${argType})`;
+      } else {
+        str = `${argType}`;
+      }
+    } else {
+      str = '()';
+    }
+    return str;
+  };
+
+  // Formats a return type, adding `Option` if the modifier is `Optional`.
+  formatReturnType = (returnType: string, modifier: string) => {
+    let str = '';
+    if (returnType !== '') {
+      str += ': ';
+
+      if (modifier === 'Optional') {
+        str += `Option<${returnType}>`;
+      } else {
+        str += returnType;
+      }
+    }
+    return str;
+  };
+
+  // ------------------------------------------------------
+  // Recursive type formatting.
+  // ------------------------------------------------------
+
+  // A recursive function that formats a call signature by formatting its arguments and return
+  // types.
+  getTypeString = (section: AnyJson) => {
+    let str = '';
+
+    switch (section?.type) {
+      case 'array':
+        str = this.getTypeString(section.array.type);
+        break;
+
+      case 'compact':
+        str = this.getTypeString(section.sequence);
+        break;
+
+      case 'sequence':
+        str = `Vec<${this.getTypeString(section.sequence)}>`;
+        break;
+
+      case 'tuple':
+        str = `(${section.tuple.reduce(
+          (acc: string, subSection: AnyJson, index: number) => {
+            const sigType = this.getTypeString(subSection);
+            acc += sigType;
+            if (index !== section.tuple.length - 1) {
+              acc += ', ';
+            }
+            return acc;
+          },
+          ''
+        )})`;
+        break;
+
+      case 'bitSequence':
+      case 'primitive':
+        str = this.getShortLabel(section.label);
+        break;
+
+      case 'composite':
+        // Expand type if short label is not defined, or if basic types.
+        if (
+          ['', 'BoundedVec', 'WeakBoundedVec'].includes(section.label.short)
+        ) {
+          str += section.composite.reduce(
+            (acc: string, field: AnyJson, index: number) => {
+              acc = acc + this.getTypeString(field.type);
+              if (index < section.composite.length - 1) {
+                acc += ', ';
+              }
+              return acc;
+            },
+            ''
+          );
+        } else {
+          str = `${section.label.short}`;
+        }
+
+        break;
+      case 'variant':
+        str = `${section.label.short}`;
+        // If variant is `Option`, expand signature with its `Some` type.
+        if (section.label.short === 'Option') {
+          // TODO: expand to check if this variant is actually an option of `Some` and `None`, where
+          // `None` has no fields.
+          str +=
+            section.variant[1].fields.reduce(
+              (acc: string, field: AnyJson, index: number) => {
+                acc = acc + this.getTypeString(field.type);
+                if (index < section.variant[1].fields.length - 1) {
+                  acc += ', ';
+                }
+                return acc;
+              },
+              `<`
+            ) + `>`;
+        }
+        break;
+    }
+    return str;
+  };
+
+  // ------------------------------------------------------
+  // Class helpers
+  // ------------------------------------------------------
+
+  // Gets a short label from a label input.
+  getShortLabel = (input: string | { long: string; short: string }) =>
+    typeof input === 'string' ? input : input.short;
+
+  // Gets a long label from a label input.
+  getLongLabel = (input: string | { long: string; short: string }) =>
+    typeof input === 'string' ? input : input.long;
+}
--- src/routes/Chain/ChainState/index.tsx
+++ src/routes/Chain/ChainState/index.tsx
@@ -12,12 +12,12 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { Header } from './Header';
 import { useApi } from 'contexts/Api';
 import { useTabs } from 'contexts/Tabs';
-import { useCallback, useMemo, useRef, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import { useOutsideAlerter } from 'hooks/useOutsideAlerter';
 import { PalletList } from '../PalletList';
 import type { AnyJson } from '@w3ux/utils/types';
 import { PalletScraper } from 'model/Metadata/Scraper/Pallet';
-import { getShortLabel } from '../Utils';
+import { FormatCallSignature } from 'model/Metadata/Format/CallSignature';
 
 interface StorgeListItem {
   docs: string[];
@@ -54,8 +54,8 @@ export const ChainState = () => {
     ['ignore-outside-alerter-storage']
   );
 
-  // Scrapes storage items from metadata for a selected pallet and returns them in a formatted list.
-  const getStorageItems = () => {
+  // Fetch storage data when metadata or the selected pallet changes.
+  const storageData = useMemo(() => {
     if (!Metadata) {
       return {
         pallets: [],
@@ -66,151 +66,33 @@ export const ChainState = () => {
     // Get pallet list from scraper.
     const scraper = new PalletScraper(Metadata);
     const pallets = scraper.getList(['storage']);
+
+    // If no pallet selected, get first one from scraper or fall back to null.
     const activePallet = selectedPallet || pallets?.[0].name || null;
-
-    let storageItems = [];
-    if (activePallet) {
-      storageItems = scraper.getStorage(activePallet);
-    }
-    return { storageItems, activePallet, pallets };
-  };
-
-  // Fetch storage when metadata or the select pallet changes.
-  const { pallets, activePallet, storageItems } = useMemo(
-    () => getStorageItems(),
-    [selectedPallet, Metadata?.metadata]
-  );
-
-  // A function that can be called recursively to format a callSig argument and return types.
-  const getSigType = (section: AnyJson) => {
-    let typeStr = '';
-
-    switch (section?.type) {
-      case 'array':
-        typeStr = getSigType(section.array.type);
-        break;
-
-      case 'compact':
-        typeStr = getSigType(section.sequence);
-        break;
-      case 'sequence':
-        typeStr = `Vec<${getSigType(section.sequence)}>`;
-        break;
-      case 'tuple':
-        typeStr = `(${section.tuple.reduce(
-          (acc: string, subSection: AnyJson, index: number) => {
-            const sigType = getSigType(subSection);
-            acc += sigType;
-            if (index !== section.tuple.length - 1) {
-              acc += ', ';
-            }
-            return acc;
-          },
-          ''
-        )})`;
-        break;
-      case 'bitSequence':
-      case 'primitive':
-        typeStr = getShortLabel(section.label);
-        break;
-      case 'composite':
-        // Expand type if short label is not defined, or if basic types.
-        if (
-          ['', 'BoundedVec', 'WeakBoundedVec'].includes(section.label.short)
-        ) {
-          typeStr += section.composite.reduce(
-            (acc: string, field: AnyJson, index: number) => {
-              let str = acc + getSigType(field.type);
-              if (index < section.composite.length - 1) {
-                str += ', ';
-              }
-              return str;
-            },
-            ''
-          );
-        } else {
-          typeStr = `${section.label.short}`;
-        }
-
-        break;
-      case 'variant':
-        typeStr = `${section.label.short}`;
-        // If variant is `Option`, expand signature with its `Some` type.
-        if (section.label.short === 'Option') {
-          // TODO: expand to check if this variant is actually an option of `Some` and `None`, where
-          // `None` has no fields.
-          typeStr +=
-            section.variant[1].fields.reduce(
-              (acc: string, field: AnyJson, index: number) => {
-                let str = acc + getSigType(field.type);
-                if (index < section.variant[1].fields.length - 1) {
-                  str += ', ';
-                }
-                return str;
-              },
-              `<`
-            ) + `>`;
-        }
-        break;
-    }
-    return typeStr;
-  };
-
-  const storage = storageItems.map((storageItem: AnyJson) => {
-    const {
-      modifier,
-      type: { argTypes, returnType },
-    } = storageItem;
-
-    // Format arguments and return types
-    const sigTypes: [string, string] = ['', ''];
-    //               ^^^^^^  ^^^^^^
-    //               args,   return
-    [argTypes, returnType].forEach((section, index) => {
-      sigTypes[index] = getSigType(section);
-    });
-
-    let callSig = '';
-    if (sigTypes[0] !== '') {
-      // Regex to check if `sigTypes[0] is already wrapped in parens, and to do so if not.
-      if (!/^\(.*\)$/.test(sigTypes[0])) {
-        callSig = `(${sigTypes[0]})`;
-      } else {
-        callSig = `${sigTypes[0]}`;
-      }
-    } else {
-      callSig = '()';
-    }
-
-    if (sigTypes[1] !== '') {
-      callSig += `: `;
-      // Handle modifiers.
-      if (modifier === 'Optional') {
-        callSig += `Option<${sigTypes[1]}>`;
-      } else {
-        callSig += sigTypes[1];
-      }
-    }
+    const storageItems = activePallet ? scraper.getStorage(activePallet) : [];
 
     return {
-      ...storageItem,
-      callSig,
+      storageItems,
+      activePallet,
+      pallets,
     };
-  });
+  }, [selectedPallet, Metadata?.metadata]);
 
-  // List of storage items alphabetically.
-  const orderSotrageItems = useCallback(
-    (selection: StorgeListItem[]) =>
-      selection.sort(({ name: nameA }, { name: nameB }) =>
-        nameA < nameB ? -1 : nameA > nameB ? 1 : 0
-      ),
-    [storage]
+  const { pallets, activePallet, storageItems } = storageData;
+
+  // Inject call signature into storage items.
+  const storageList: StorgeListItem[] = useMemo(
+    () =>
+      storageItems
+        .map((storageItem: AnyJson) => ({
+          ...storageItem,
+          callSig: new FormatCallSignature(storageItem).format(),
+        }))
+        .sort(({ name: nameA }, { name: nameB }) =>
+          nameA < nameB ? -1 : nameA > nameB ? 1 : 0
+        ),
+    [storageItems]
   );
-
-  // Sort storage items alphabetically based on call name.
-  let selection: StorgeListItem[] = storage;
-
-  selection = useMemo(() => orderSotrageItems(selection), [orderSotrageItems]);
 
   return (
     <>
@@ -233,12 +115,12 @@ export const ChainState = () => {
             >
               <span>
                 <SelectTextWrapper>
-                  {selection[0]?.name || 'No Storage Items'}
-                  <span>{selection[0]?.callSig || ''}</span>
+                  {storageList[0]?.name || 'No Storage Items'}
+                  <span>{storageList[0]?.callSig || ''}</span>
                 </SelectTextWrapper>
               </span>
               <span>
-                <h5>{selection[0]?.docs?.[0] || ''}</h5>
+                <h5>{storageList[0]?.docs?.[0] || ''}</h5>
                 <FontAwesomeIcon icon={faChevronDown} transform="shrink-4" />
               </span>
             </SelectItemWrapper>
@@ -247,7 +129,7 @@ export const ChainState = () => {
               ref={storageSelectRef}
               className={`${storageOpen ? ` open` : ``}`}
             >
-              {selection.map(({ name, docs, callSig }) => (
+              {storageList.map(({ name, docs, callSig }) => (
                 <SelectItemWrapper
                   key={`storage_select_${name}`}
                   className="option"
