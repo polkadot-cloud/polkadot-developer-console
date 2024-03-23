a897e8711a784bcc437c3add4ecc1edb1462be33
Ross Bulat <ross@parity.io>
2024-03-23 14:31:00 +0700

    add license

--- src/library/Buttons/ButtonSubmit.tsx
+++ src/library/Buttons/ButtonSubmit.tsx
@@ -1,3 +1,6 @@
+// Copyright 2024 @rossbulat/console authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
 import styled from 'styled-components';
 
 export const ButtonSubmit = styled.button`
