8c84a70d256a9c8c83df3637d79b48a2fdcb1980
Ross Bulat <ross@parity.io>
2024-03-25 14:46:09 +0700

    add base58check test

--- src/routes/Chain/Overview/index.tsx
+++ src/routes/Chain/Overview/index.tsx
@@ -12,8 +12,6 @@ import BigNumber from 'bignumber.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHive } from '@fortawesome/free-brands-svg-icons';
 import { faList } from '@fortawesome/free-solid-svg-icons';
-import { base58Decode, base58Encode } from '@polkadot/util-crypto';
-import { u8aToHex, u8aToNumber } from '@polkadot/util';
 
 export const Overview = () => {
   const { getApiStatus, getChainSpec } = useApi();
@@ -22,21 +20,6 @@ export const Overview = () => {
   const apiStatus = getApiStatus(activeTabId);
   const chainSpec = getChainSpec(activeTabId);
   const chainSpecReady = !!chainSpec;
-
-  const decoded = base58Decode(
-    '1hYiMW8KSfUYChzCQSPGXvMSyKVqmyvMXqohjKr3oU5PCXF'
-  );
-  const decodedHex = u8aToHex(decoded);
-
-  const ss58 = u8aToNumber(decoded.subarray(0, 1));
-  const publicKey = u8aToHex(decoded.subarray(2));
-
-  console.log('full hex: ', decodedHex);
-  console.log('ss58 prefix: ', ss58);
-  console.log('public key: ', publicKey);
-
-  const encodedKey = base58Encode(decoded);
-  console.log('encoding back: ', encodedKey);
 
   // NOTE: we know for certain there is an active tab and an associated API instance here, so we can
   // safely use the non-null assertion.
