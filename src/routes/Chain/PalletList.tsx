e87648a99f60270a60956b6f0d6774457240072e
Ross Bulat <ross@parity.io>
2024-03-27 17:58:22 +0700

    navigate pallet list with keys

--- src/routes/Chain/PalletList.tsx
+++ src/routes/Chain/PalletList.tsx
@@ -13,9 +13,10 @@ import { useOutsideAlerter } from 'hooks/useOutsideAlerter';
 import { SearchWrapper } from 'library/ContextMenu/Wrappers';
 import { formatInputString } from 'Utils';
 import { useChainUi } from 'contexts/ChainUi';
-import { camelize } from '@w3ux/utils';
+import { camelize, setStateWithRef } from '@w3ux/utils';
 import { useActiveTabId } from 'contexts/ActiveTab';
 import type { PalletListProps } from './ChainState/types';
+import { useEventListener } from 'usehooks-ts';
 
 export const PalletList = ({
   pallets,
@@ -25,16 +26,18 @@ export const PalletList = ({
 }: PalletListProps) => {
   const activeTabId = useActiveTabId();
   const { getPalletVersions, getChainUi, setChainUiItem } = useChainUi();
+
   const palletVersions = getPalletVersions(activeTabId) || {};
   const chainUi = getChainUi(activeTabId, chainUiSection);
   const { palletSearch } = chainUi;
 
   // Pallet selection open.
   const [palletsOpen, setPalletsOpenState] = useState<boolean>(false);
+  const palletsOpenRef = useRef(palletsOpen);
 
   // Setter for pallet menu open state.
   const setPalletsOpen = (value: boolean) => {
-    setPalletsOpenState(value);
+    setStateWithRef(value, setPalletsOpenState, palletsOpenRef);
   };
 
   // Handle pallet search change.
@@ -56,6 +59,31 @@ export const PalletList = ({
   // Pallet search input ref.
   const searchInputRef = useRef<HTMLInputElement>(null);
 
+  // Handle key down events
+  const handleKeyDown = (ev: KeyboardEvent) => {
+    const { type, key } = ev;
+    const palletIndex = filteredPallets.findIndex(
+      ({ name }) => name === activePallet
+    );
+
+    // Determine the new pallet index, defaulting to the active pallet if present in the filtered
+    // list, otherwise 0
+    let newIndex = palletIndex > filteredPallets.length - 1 ? 0 : palletIndex;
+    if (palletsOpenRef.current && type === 'keydown') {
+      if (key === 'ArrowDown') {
+        newIndex = Math.min(newIndex + 1, filteredPallets.length - 1);
+      } else if (key === 'ArrowUp') {
+        newIndex = Math.max(newIndex - 1, 0);
+      }
+
+      // Update the active pallet if the index points to a valid filtered pallet.
+      const newActivePallet = filteredPallets[newIndex]?.name;
+      if (newActivePallet) {
+        setChainUiItem(activeTabId, chainUiSection, 'pallet', newActivePallet);
+      }
+    }
+  };
+
   // Close pallet selection if clicked outside of its container.
   useOutsideAlerter(
     palletSelectRef,
@@ -71,6 +99,10 @@ export const PalletList = ({
       searchInputRef.current?.focus();
     }
   }, [palletsOpen]);
+
+  // Listen for key down events for form control.
+  const documentRef = useRef(document);
+  useEventListener('keydown', handleKeyDown, documentRef);
 
   return (
     <section>
