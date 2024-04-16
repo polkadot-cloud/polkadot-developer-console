e55369d53a6b1ca4906aef57c64b53840aa78062
Ross Bulat <ross@parity.io>
2024-04-16 22:03:42 +0700

    subscriptions keyed by tabId

--- src/controllers/Subscriptions/index.ts
+++ src/controllers/Subscriptions/index.ts
@@ -1,7 +1,6 @@
 // Copyright 2024 @rossbulat/console authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import type { ChainId } from 'config/networks';
 import type { ChainSubscriptions, Subscription } from './types';
 
 // A class to manage subscriptions.
@@ -11,44 +10,59 @@ export class SubscriptionsController {
   // Class members.
   // ------------------------------------------------------
 
-  // Subscription objects, keyed by a chainId.
-  static #subs: Partial<Record<ChainId, ChainSubscriptions>> = {};
+  // Subscription objects, keyed by a tabId.
+  static #subs: Partial<Record<string, ChainSubscriptions>> = {};
 
   // ------------------------------------------------------
-  // Getter.
+  // Getters.
   // ------------------------------------------------------
 
-  // Get a subscription by chainId and subscriptionId.
-  static get(
-    chainId: ChainId,
-    subscriptionId: string
-  ): Subscription | undefined {
-    const chainSubs = this.#subs[chainId];
-    return chainSubs ? chainSubs[subscriptionId] : undefined;
+  // Gets all subscriptions for a tab.
+  static getAll(tabId: number): ChainSubscriptions | undefined {
+    return this.#subs[String(tabId)];
+  }
+
+  // Get a subscription by tabId and subscriptionId.
+  static get(tabId: number, subscriptionId: string): Subscription | undefined {
+    return this.#subs[String(tabId)]?.[subscriptionId] || undefined;
   }
 
   // ------------------------------------------------------
   // Setter.
   // ------------------------------------------------------
 
-  // Sets a new subscription for a `chainId`.
+  // Sets a new subscription for a `tabId`.
   static set(
-    chainId: ChainId,
+    tabId: number,
     subscriptionId: string,
     subscription: Subscription
   ): void {
-    // Ignore if there is already a subscription for this chainId and subscriptionId.
-    if (this.#subs?.[chainId]?.[subscriptionId]) {
+    // Ignore if there is already a subscription for this tabId and subscriptionId.
+    if (this.#subs?.[String(tabId)]?.[subscriptionId]) {
       return;
     }
 
-    if (!this.#subs[chainId]) {
-      this.#subs[chainId] = {};
+    // Create a new subscriptions record for the tab if one doesn't exist.
+    if (!this.#subs[tabId]) {
+      this.#subs[String(tabId)] = {};
     }
 
-    if (this.#subs[chainId]) {
-      // NOTE: We know for certain that `this.#subs[chainId]` is defined here.
-      this.#subs[chainId]!.subscriptionId = subscription;
+    // NOTE: We know for certain that `this.#subs[tabId]` is defined here.
+    this.#subs[String(tabId)]![subscriptionId] = subscription;
+  }
+
+  // ------------------------------------------------------
+  // Unsubscribe.
+  // ------------------------------------------------------
+
+  // Unsubscribe from a subscription.
+  static async removeSub(tabId: number, subscriptionId: string): Promise<void> {
+    if (this.#subs[String(tabId)]) {
+      try {
+        delete this.#subs[String(tabId)]![subscriptionId];
+      } catch (e) {
+        // Silently fail if the subscription doesn't exist.
+      }
     }
   }
 }
