af0c86206f0e3cd225881e12863481a6a6377f65
Ross Bulat <ross@parity.io>
2024-04-16 21:58:54 +0700

    add block number subscription

---
+++ src/model/BlockNumber/types.ts
@@ -0,0 +1,10 @@
+// Copyright 2024 @rossbulat/console authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import type { ChainId } from 'config/networks';
+
+export interface BlockNumberEventDetail {
+  blockNumber: number;
+  chainId: ChainId;
+  tabId: number;
+}
