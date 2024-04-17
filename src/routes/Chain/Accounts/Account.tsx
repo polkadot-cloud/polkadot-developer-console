adfc63b9e7ea52a01fd7bde08bcc0f5f8e5f4f5a
Ross Bulat <ross@parity.io>
2024-04-17 14:20:34 +0700

    basic account balance support

--- src/routes/Chain/Accounts/Account.tsx
+++ src/routes/Chain/Accounts/Account.tsx
@@ -1,7 +1,7 @@
 // Copyright 2024 @rossbulat/console authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import { ellipsisFn, remToUnit } from '@w3ux/utils';
+import { ellipsisFn, planckToUnit, remToUnit } from '@w3ux/utils';
 import type { AccountProps } from './types';
 import { Polkicon } from '@w3ux/react-polkicon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,15 +10,27 @@ import { useMenu } from 'contexts/Menu';
 import { AccountContextMenu } from './AccountMenu';
 import { useTabs } from 'contexts/Tabs';
 import { useActiveTabId } from 'contexts/ActiveTab';
+import { useAccounts } from 'contexts/Accounts';
+import BigNumber from 'bignumber.js';
+import type { DirectoryId } from 'config/networks';
+import { NetworkDirectory } from 'config/networks';
 
 export const Account = ({ account, chainId }: AccountProps) => {
   const { getTab } = useTabs();
   const { openMenu } = useMenu();
   const activeTabId = useActiveTabId();
-
-  const tab = getTab(activeTabId);
+  const { getAccountBalance } = useAccounts();
 
   const { name, address } = account;
+  const tab = getTab(activeTabId);
+  const balances = getAccountBalance(address);
+
+  // TODO: Deduct account locks.
+  const freePlanck = balances?.balance?.free || new BigNumber(0);
+
+  // NOTE: assuming chain definitely exists here.
+  // TODO: Move chain unit and ss58 prefix to tab settings and assign for customEndpoint also.
+  const unit = chainId ? NetworkDirectory[chainId as DirectoryId].unit : 'UNIT';
 
   return (
     <section>
@@ -54,6 +66,12 @@ export const Account = ({ account, chainId }: AccountProps) => {
               >
                 <FontAwesomeIcon icon={faCopy} transform="shrink-4" />
               </button>
+            </h5>
+          </div>
+          <div className="free">
+            <h5>
+              <span>Free:</span>{' '}
+              {planckToUnit(freePlanck, 10).decimalPlaces(3).toFormat()} {unit}
             </h5>
           </div>
         </div>
