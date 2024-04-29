cd6dfd4b0862cce603d6f5b52c7b8a468a222dc7
Ross Bulat <ross@parity.io>
2024-04-29 10:35:48 +0700

    add Textbox placeholder

--- src/routes/Chain/Inputs/Textbox.tsx
+++ src/routes/Chain/Inputs/Textbox.tsx
@@ -32,6 +32,7 @@ export const Textbox = ({
       <h4>{label}</h4>
       <TextInputWrapper className="input">
         <input
+          placeholder="..."
           type="text"
           value={value || ''}
           onChange={(ev) => handleTextboxChange(ev.currentTarget.value)}
