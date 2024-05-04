017304092ef5a65bdb20bd75d08645a45437c1e1
Ross Bulat <ross@parity.io>
2024-05-04 19:35:13 +0700

    remove unused props

--- src/routes/Chain/Inputs/index.tsx
+++ src/routes/Chain/Inputs/index.tsx
@@ -304,7 +304,6 @@ export const useInput = () => {
           return (
             <Section indent={indent}>
               <Select
-                {...inputArgConfig}
                 label={label}
                 values={values || []}
                 value={
