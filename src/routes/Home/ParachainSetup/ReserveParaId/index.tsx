d0952dfe2d1382fef216a709bdf4f85d23bced07
Ross Bulat <ross@parity.io>
2024-05-06 15:37:09 +0700

    chore: reserve para id account list

--- src/routes/Home/ParachainSetup/ReserveParaId/index.tsx
+++ src/routes/Home/ParachainSetup/ReserveParaId/index.tsx
@@ -1,15 +1,28 @@
 // Copyright 2024 @rossbulat/console authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
+import { AccountId32 } from 'library/Inputs/AccountId32';
 import { FormWrapper } from '../Wrappers';
 import type { StepProps } from '../types';
+import { useImportedAccounts } from 'contexts/ImportedAccounts';
 
-export const ReserveParaId = (props: StepProps) => {
-  console.log(props);
+export const ReserveParaId = ({ relayInstance }: StepProps) => {
+  const { getAccounts } = useImportedAccounts();
+
+  const chainSpec = relayInstance?.chainSpec;
+
+  const accounts =
+    chainSpec && chainSpec.chain
+      ? getAccounts(chainSpec.chain, chainSpec.ss58Prefix)
+      : [];
 
   return (
     <FormWrapper>
       <h3>Reserve a Para ID or select an existing one from your accounts.</h3>
+
+      <section>
+        <AccountId32 accounts={accounts} />
+      </section>
     </FormWrapper>
   );
 };
