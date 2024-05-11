a2c9c8335e15f6506de55aaef8a5223ae3e1112f
Ross Bulat <ross@parity.io>
2024-05-11 22:54:09 +0700

    re-org tab types & defaults

--- src/contexts/Tabs/types.ts
+++ src/contexts/Tabs/types.ts
@@ -8,6 +8,49 @@ import type { Dispatch, SetStateAction } from 'react';
 // The tasks that developer console supports.
 export type TabTask = 'chainBrowser' | 'parachainSetup';
 
+export interface TabsContextInterface {
+  tabs: Tabs;
+  tabsRef: Tabs;
+  selectedTabId: number;
+  dragId: number | null;
+  tabHoverIndex: number;
+  activeTabIndex: number;
+  instantiatedIds: number[];
+  redirectCounter: number;
+  setTabs: (tabs: Tabs) => void;
+  setSelectedTabId: (index: number) => void;
+  createTab: () => void;
+  getTab: (id: number) => Tab | undefined;
+  destroyTab: (index: number, id: number) => void;
+  setTabHoverIndex: Dispatch<SetStateAction<number>>;
+  setSelectedTabIndex: (index: number) => void;
+  addInstantiatedId: (id: number) => void;
+  setDragId: Dispatch<SetStateAction<number | null>>;
+  renameTab: (id: number, name: string) => void;
+  incrementRedirectCounter: () => void;
+  setTabAutoConnect: (id: number, autoConnect: boolean) => void;
+  setTabConnectFrom: (tabId: number, connectFrom: ConnectFrom) => void;
+  setTabActivePage: (
+    id: number,
+    route: Route,
+    activePage: number,
+    persist?: boolean
+  ) => void;
+  switchTab: (tabId: number, tabIndex: number) => void;
+  getTabActiveTask: (tabId: number) => TabTask | null;
+  setTabActiveTask: (tabId: number, task: TabTask | null) => void;
+  getTabTaskData: (tabId: number) => TaskData;
+  setTabTaskData: (tabId: number, value: TaskData) => void;
+  getTabChainSpaceApiIndexes: (tabId: number) => ChainSpaceIndex[];
+  getChainSpaceApiIndex: (
+    tabId: number,
+    label: string
+  ) => ChainSpaceIndex | undefined;
+  setChainSpaceApiIndex: (tabId: number, index: ChainSpaceIndex) => void;
+  removeTabChainSpaceIndexes: (tabId: number) => void;
+  removeChainSpaceApiIndex: (tabId: number, label: string) => void;
+}
+
 export type Tabs = Tab[];
 
 export interface Tab {
@@ -48,49 +91,6 @@ export interface ChainMeta {
 }
 
 export type ConnectFrom = 'directory' | 'customEndpoint';
-
-export interface TabsContextInterface {
-  tabs: Tabs;
-  tabsRef: Tabs;
-  setTabs: (tabs: Tabs) => void;
-  createTab: () => void;
-  selectedTabId: number;
-  getTab: (id: number) => Tab | undefined;
-  destroyTab: (index: number, id: number) => void;
-  setSelectedTabId: (index: number) => void;
-  tabHoverIndex: number;
-  setTabHoverIndex: Dispatch<SetStateAction<number>>;
-  activeTabIndex: number;
-  setSelectedTabIndex: (index: number) => void;
-  addInstantiatedId: (id: number) => void;
-  setDragId: Dispatch<SetStateAction<number | null>>;
-  dragId: number | null;
-  instantiatedIds: number[];
-  renameTab: (id: number, name: string) => void;
-  redirectCounter: number;
-  incrementRedirectCounter: () => void;
-  setTabAutoConnect: (id: number, autoConnect: boolean) => void;
-  setTabConnectFrom: (tabId: number, connectFrom: ConnectFrom) => void;
-  setTabActivePage: (
-    id: number,
-    route: Route,
-    activePage: number,
-    persist?: boolean
-  ) => void;
-  switchTab: (tabId: number, tabIndex: number) => void;
-  getTabActiveTask: (tabId: number) => TabTask | null;
-  setTabActiveTask: (tabId: number, task: TabTask | null) => void;
-  getTabTaskData: (tabId: number) => TaskData;
-  setTabTaskData: (tabId: number, value: TaskData) => void;
-  getTabChainSpaceApiIndexes: (tabId: number) => ChainSpaceIndex[];
-  getChainSpaceApiIndex: (
-    tabId: number,
-    label: string
-  ) => ChainSpaceIndex | undefined;
-  setChainSpaceApiIndex: (tabId: number, index: ChainSpaceIndex) => void;
-  removeTabChainSpaceIndexes: (tabId: number) => void;
-  removeChainSpaceApiIndex: (tabId: number, label: string) => void;
-}
 
 // Active pages structure used in local storage to keep track of active page indexes for each tab.
 export type TabsActivePages = Record<string, TabActivePages> | undefined;
