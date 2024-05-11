09d1275a91f190551f23dc3f369d3139e9a951a1
Ross Bulat <ross@parity.io>
2024-05-11 11:15:12 +0700

    add useSetActivePage hook

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,39 +6,18 @@ import { Settings } from 'routes/Settings';
 import { Chain } from 'routes/Chain';
 import { Default } from 'routes/Home';
 import { useActiveTab } from 'contexts/ActiveTab';
-import { useEffect } from 'react';
 import { useTabs } from 'contexts/Tabs';
-import * as local from 'contexts/Tabs/Local';
+import { useSetActivePage } from 'hooks/useSetActivePage';
 
 export const Router = () => {
   const { tab, tabId } = useActiveTab();
-  const { setTabActivePage, getTabActiveTask } = useTabs();
+  const { getTabActiveTask } = useTabs();
+
   const tabActiveTask = getTabActiveTask(tabId);
 
-  // Upon an activeTask change, check if a local active page exists for that route, and
-  // move to it if so.
-  const onTaskUpdated = () => {
-    const activePage = local.getActivePage(tabId, 'default');
-    if (tab && activePage !== undefined && activePage !== tab.activePage) {
-      // TODO: Amend to take into consideration the `activeTask`, rather than `apiActive`.
-      setTabActivePage(tabId, 'default', activePage, true);
-    }
-  };
-
-  // Redirect to local default page on disconnect if activeTask is no longer assigned.
-  const onTaskRemoved = () => {
-    const localActivePage = local.getActivePage(tabId, 'default');
-    setTabActivePage(tabId, 'default', localActivePage || 0);
-  };
-
-  // Handle active page changes on tab and `activeTask` changes.
-  useEffect(() => {
-    if (tabActiveTask === null) {
-      onTaskRemoved();
-    } else {
-      onTaskUpdated();
-    }
-  }, [tabId, tabActiveTask]);
+  // on tab / location / task changes, check if a local active page exists for that route, and move
+  // to that page if so.
+  useSetActivePage();
 
   return (
     <Routes>
