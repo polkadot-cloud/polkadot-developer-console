8755fbf384b713d24a57989579aa383e017708ae
Ross Bulat <ross@parity.io>
2024-05-13 15:28:49 +0700

    rm api related logic from networkColor

--- src/routes/Common/PageWithMenu/index.tsx
+++ src/routes/Common/PageWithMenu/index.tsx
@@ -9,9 +9,7 @@ import { accentColors } from 'styles/accents/developer-console';
 import { useSettings } from 'contexts/Settings';
 import { PageWrapper } from 'library/PageContent/Wrappers';
 import { useActiveTab } from 'contexts/ActiveTab';
-import { useChainSpaceEnv } from 'contexts/ChainSpaceEnv';
 import { useTabs } from 'contexts/Tabs';
-import { useApiIndexer } from 'contexts/ApiIndexer';
 import { PageContent } from 'library/PageContent';
 
 // Renders a page and menu, with state controlling the active section of the page.
@@ -20,30 +18,25 @@ export const PageWithMenu = ({
   Menu,
   routeProvider,
 }: PageWithMenuProps) => {
+  const { tab } = useActiveTab();
   const routeConfig = routeProvider();
   const { getTabActiveTask } = useTabs();
-  const { tab, ownerId } = useActiveTab();
-  const { getTabApiIndex } = useApiIndexer();
   const { chainColorEnabled } = useSettings();
-  const { getApiStatus } = useChainSpaceEnv();
 
   // Redirect when redirects are present in local storage.
   useRedirect({ route });
 
-  const apiInstanceId = getTabApiIndex(ownerId, 'chainExplorer')?.instanceId;
   const activeTask = tab?.id && getTabActiveTask(tab.id);
-  const apiStatus = getApiStatus(apiInstanceId);
 
   // Get colors from active chain id.
   const chainId: DirectoryId | undefined =
     (tab?.taskData?.chain?.id as DirectoryId) || undefined;
 
-  const apiConnected = ['ready', 'connected', 'connecting'].includes(apiStatus);
   const networkColor = NetworkDirectory[chainId]?.color;
 
   // Get chain color, if present.
   const getChainColor = () =>
-    activeTask !== 'chainExplorer' || !apiConnected || !chainColorEnabled
+    activeTask !== 'chainExplorer' || !chainColorEnabled
       ? accentColors.primary.light
       : networkColor
         ? networkColor
