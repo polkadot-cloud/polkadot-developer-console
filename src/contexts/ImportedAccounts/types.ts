41726b1cf30bef2c253f1aa69acd514e26f55397
Ross Bulat <ross@parity.io>
2024-05-29 19:44:37 +0700

    add types to ImportedAccounts

--- src/contexts/ImportedAccounts/types.ts
+++ src/contexts/ImportedAccounts/types.ts
@@ -5,7 +5,17 @@ import type { ImportedAccount } from '@w3ux/react-connect-kit/types';
 
 export interface ImportedAccountsContextInterface {
   getAccounts: (chainId: string, ss58Preifx: number) => ImportedAccount[];
+  getAccount: (
+    address: string,
+    chainId: string,
+    ss58Prefix: number
+  ) => ImportedAccount | undefined;
   accountHasSigner: (
+    address: string,
+    chainId: string,
+    ss58Prefix: number
+  ) => boolean;
+  requiresManualSign: (
     address: string,
     chainId: string,
     ss58Prefix: number
