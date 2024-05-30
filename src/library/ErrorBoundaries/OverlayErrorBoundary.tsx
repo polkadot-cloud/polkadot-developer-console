5f5157ef9401ab7b6aaab6f98dcdca4fcb25e528
Ross Bulat <ross@parity.io>
2024-05-30 10:57:49 +0700

    add license

--- src/library/ErrorBoundaries/OverlayErrorBoundary.tsx
+++ src/library/ErrorBoundaries/OverlayErrorBoundary.tsx
@@ -1,3 +1,6 @@
+// Copyright 2024 @polkadot-developer-console/polkadot-developer-console authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
 import type { FC } from 'react';
 import { Wrapper } from './Wrapper';
 
