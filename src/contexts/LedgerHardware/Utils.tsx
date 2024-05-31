902603a1c49bc76d842d3dac8a05a234ddd5d140
Ross Bulat <ross@parity.io>
2024-05-31 13:20:22 +0700

    add Ledger contexts boilerplate

---
+++ src/contexts/LedgerHardware/Utils.tsx
@@ -0,0 +1,58 @@
+// Copyright 2024 @polkadot-developer-console/polkadot-developer-console authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { localStorageOrDefault } from '@w3ux/utils';
+import type { LedgerAddress } from './types';
+import { LedgerApps } from './defaults';
+
+// Ledger error keyed by type of error.
+const LedgerErrorsByType = {
+  timeout: ['Error: Timeout'],
+  methodNotSupported: ['Error: Method not supported'],
+  nestingNotSupported: ['Error: Call nesting not supported'],
+  outsideActiveChannel: ['Error: TransportError: Invalid channel'],
+  deviceNotConnected: ['TransportOpenUserCancelled'],
+  deviceBusy: ['Error: Ledger Device is busy', 'InvalidStateError'],
+  deviceLocked: ['Error: LockedDeviceError'],
+  transactionRejected: ['Error: Transaction rejected'],
+  txVersionNotSupported: ['Error: Txn version not supported'],
+  appNotOpen: ['Error: Unknown Status Code: 28161'],
+};
+
+// Determine type of error returned by Ledger.
+export const getLedgerErrorType = (err: string) => {
+  let errorType = null;
+  Object.entries(LedgerErrorsByType).every(([type, errors]) => {
+    let found = false;
+    errors.every((e) => {
+      if (err.startsWith(e)) {
+        errorType = type;
+        found = true;
+        return false;
+      }
+      return true;
+    });
+    if (found) {
+      return false;
+    }
+    return true;
+  });
+  return errorType || 'misc';
+};
+
+// Gets ledger app from local storage, fallback to first entry.
+export const getLedgerApp = (network: string) =>
+  LedgerApps.find((a) => a.network === network) || LedgerApps[0];
+
+// Gets saved ledger addresses from local storage.
+export const getLocalLedgerAddresses = (network?: string) => {
+  const localAddresses = localStorageOrDefault(
+    'ledger_addresses',
+    [],
+    true
+  ) as LedgerAddress[];
+
+  return network
+    ? localAddresses.filter((a) => a.network === network)
+    : localAddresses;
+};
