cff88a4a8d00a8848e9c9ffa77fd1d77584ee73d
Ross Bulat <ross@parity.io>
2024-06-02 10:58:01 +0700

    add license

--- src/routes/Chain/ChainState/Results/Utils.ts
+++ src/routes/Chain/ChainState/Results/Utils.ts
@@ -1,3 +1,6 @@
+// Copyright 2024 @polkadot-developer-console/polkadot-developer-console authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
 import type { AnyJson } from '@w3ux/utils/types';
 
 export const formatJSON = (obj: AnyJson, indentLevel = 0) => {
