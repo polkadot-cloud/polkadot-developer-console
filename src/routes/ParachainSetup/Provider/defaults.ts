200bbb397ad0bd2a6122387e5da4a55975706d12
Ross Bulat <ross@parity.io>
2024-06-03 15:22:04 +0700

    use dummy ApiPromise

--- src/routes/ParachainSetup/Provider/defaults.ts
+++ src/routes/ParachainSetup/Provider/defaults.ts
@@ -1,7 +1,7 @@
 // Copyright 2024 @polkadot-developer-console/polkadot-developer-console authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import { ApiPromise } from '@polkadot/api';
+import type { ApiPromise } from '@polkadot/api';
 import { dummyChainSpec } from 'contexts/ChainSpaceEnv/defaults';
 import { dummyChain } from 'routes/Chain/Provider/defaults';
 
@@ -9,5 +9,5 @@ export const defaultParachainContextInterface = {
   chain: dummyChain,
   chainSpec: dummyChainSpec,
   instanceId: 'tab_0_0',
-  api: new ApiPromise(),
+  api: {} as ApiPromise,
 };
