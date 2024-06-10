7899c87ec8648902b0fda6fbbc45e2b093764c02
Ross Bulat <ross@parity.io>
2024-06-10 14:04:42 +0700

    add test metadata, test comments

---
+++ test/metadata/lookup.spec.ts
@@ -0,0 +1,23 @@
+// Copyright 2024 @polkadot-cloud/polkadot-developer-console authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import assert from 'assert';
+
+/* Metadata look tests.
+
+This test file verifies the structure and integrity of the metadata lookup object.
+
+The goal of this test suit is to document how lookups are structured to the developer. */
+
+// TODO: Reformat scraper to add `Lookup.ts` file, that deals with lookup queries only.
+
+// Basic lookup structure.
+describe('Basic variant structure is intact ', () => {
+  // TODO: implement.
+
+  // lookup -> types -> [id, type].
+
+  // ensure length of lookup is what is expected.
+
+  assert.equal(1, 1);
+});
