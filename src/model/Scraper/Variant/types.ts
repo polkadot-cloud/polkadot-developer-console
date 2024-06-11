cd2909b02825c9e07e0fb75ce2344fce4d78824b
Ross Bulat <ross@parity.io>
2024-06-11 16:01:53 +0700

    add license

--- src/model/Scraper/Variant/types.ts
+++ src/model/Scraper/Variant/types.ts
@@ -1,3 +1,6 @@
+// Copyright 2024 @polkadot-cloud/polkadot-developer-console authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
 import type { AnyJson } from '@w3ux/types';
 
 export interface VariantType {
