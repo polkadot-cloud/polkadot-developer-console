4a06c1e57b7df865bfd6a7e0d1fb7d4767108f0a
Ross Bulat <ross@jkrb.io>
2024-06-26 17:55:08 +0700

    add section

--- src/routes/ParachainSetup/SetupForm/GetCoretime/index.tsx
+++ src/routes/ParachainSetup/SetupForm/GetCoretime/index.tsx
@@ -18,50 +18,52 @@ export const GetCoretime = () => (
       </section>
     </Prompt>
 
-    <h3>Coretime Marketplace Platforms:</h3>
+    <section>
+      <h3>Coretime Marketplace Platforms:</h3>
 
-    <ParaIdOptionsWrapper>
-      <section>
-        <div className={`inner`}>
-          <button onClick={() => window.open('https://www.lastic.xyz/')}>
-            <h1 className="standalone">Lastic</h1>
-          </button>
+      <ParaIdOptionsWrapper>
+        <section>
+          <div className={`inner`}>
+            <button onClick={() => window.open('https://www.lastic.xyz/')}>
+              <h1 className="standalone">Lastic</h1>
+            </button>
 
-          <button
-            className="foot"
-            onClick={() => window.open('https://www.lastic.xyz/')}
-          >
-            <span>
-              <h4>
-                https://www.lastic.xyz{' '}
-                <FontAwesomeIcon icon={faExternalLink} transform="shrink-4" />
-              </h4>
-            </span>
-            <span></span>
-          </button>
-        </div>
-      </section>
+            <button
+              className="foot"
+              onClick={() => window.open('https://www.lastic.xyz/')}
+            >
+              <span>
+                <h4>
+                  https://www.lastic.xyz{' '}
+                  <FontAwesomeIcon icon={faExternalLink} transform="shrink-4" />
+                </h4>
+              </span>
+              <span></span>
+            </button>
+          </div>
+        </section>
 
-      <section>
-        <div className={`inner`}>
-          <button onClick={() => window.open('https://app.regionx.tech/')}>
-            <h1 className="standalone">RegionX</h1>
-          </button>
+        <section>
+          <div className={`inner`}>
+            <button onClick={() => window.open('https://app.regionx.tech/')}>
+              <h1 className="standalone">RegionX</h1>
+            </button>
 
-          <button
-            className="foot"
-            onClick={() => window.open('https://app.regionx.tech/')}
-          >
-            <span>
-              <h4>
-                https://app.regionx.tech{' '}
-                <FontAwesomeIcon icon={faExternalLink} transform="shrink-4" />
-              </h4>
-            </span>
-            <span></span>
-          </button>
-        </div>
-      </section>
-    </ParaIdOptionsWrapper>
+            <button
+              className="foot"
+              onClick={() => window.open('https://app.regionx.tech/')}
+            >
+              <span>
+                <h4>
+                  https://app.regionx.tech{' '}
+                  <FontAwesomeIcon icon={faExternalLink} transform="shrink-4" />
+                </h4>
+              </span>
+              <span></span>
+            </button>
+          </div>
+        </section>
+      </ParaIdOptionsWrapper>
+    </section>
   </FormWrapper>
 );
