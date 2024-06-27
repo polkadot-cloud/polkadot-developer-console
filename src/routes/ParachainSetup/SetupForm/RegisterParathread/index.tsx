208c862d0a9b0949025267affd5409d5f5072b31
Ross Bulat <ross@jkrb.io>
2024-06-27 13:20:56 +0700

    add parathread register extrinsic

--- src/routes/ParachainSetup/SetupForm/RegisterParathread/index.tsx
+++ src/routes/ParachainSetup/SetupForm/RegisterParathread/index.tsx
@@ -13,14 +13,21 @@ import { Label } from 'library/Inputs/Label';
 import { Prompt } from '../Prompt';
 import { Hash } from 'library/Inputs/Hash';
 import { useRegisterParathread } from 'contexts/ParaSetup/RegisterParathread';
+import { useSubmitExtrinsic } from 'hooks/useSubmitExtrinsic';
+import { SubmitTx } from 'library/SubmitTx';
 
 export const RegisterParathread = () => {
-  const { chainSpec } = useParachain();
   const { tabId, metaKey } = useActiveTab();
   const { getAccounts } = useImportedAccounts();
+  const { chainSpec, api, instanceId, chain } = useParachain();
   const { getSelectedAccount, validateParaId } = useReserveParaId();
   const { getRuntimeValue, setRuntimeValue, getGenesisState, setGenesisState } =
     useRegisterParathread();
+
+  const chainId = chain.id;
+  const { ss58, units, unit } = chain;
+  const { transactionVersion } = chainSpec.version;
+  const { existentialDeposit } = chainSpec.consts;
 
   const accounts = chainSpec
     ? getAccounts(chainSpec.version.specName, chainSpec.ss58Prefix)
@@ -32,10 +39,50 @@ export const RegisterParathread = () => {
   // Get whether a valid para id has been entered.
   const isValidParaId = validateParaId(tabId, selectedAccount);
 
+  // Get any entered runtime value.
+  const runtimeValue = getRuntimeValue(tabId) || '';
+  const genesisState = getGenesisState(tabId) || '';
+
   // Get the selected account if a valid para id has been entered, otherwise return an empty array.
   const registrantInputValues = !isValidParaId
     ? []
     : accounts.filter((account) => account.address === selectedAccount);
+
+  // Whether form is valid for tx submission.
+  const valid = !(!selectedAccount || !runtimeValue || !genesisState);
+
+  // Format transaction for registering parathread to submit, or return `null` if invalid.
+  const getTx = () => {
+    let tx = null;
+
+    if (!api || !valid) {
+      return tx;
+    }
+
+    try {
+      // Construct transaction.
+      // TODO: Get the submitted para ID (new or existing).
+      const paraId = 0;
+      tx = api.tx.registrar.regsiter(paraId, genesisState, runtimeValue);
+      return tx;
+    } catch (e) {
+      return null;
+    }
+  };
+
+  // Prepare extrinsic for reserving the next para id.
+  const submitExtrinsic = useSubmitExtrinsic({
+    instanceId,
+    api,
+    chainId,
+    ss58Prefix: ss58,
+    tx: getTx(),
+    from: selectedAccount,
+    shouldSubmit: true,
+    callbackSubmit: () => {
+      // TODO: Handle successful submission (parathread registered state).
+    },
+  });
 
   return (
     <FormWrapper>
@@ -80,7 +127,7 @@ export const RegisterParathread = () => {
           onChange={(val) => {
             setRuntimeValue(tabId, val);
           }}
-          value={getRuntimeValue(tabId) || ''}
+          value={runtimeValue}
         />
       </section>
 
@@ -90,7 +137,20 @@ export const RegisterParathread = () => {
           onChange={(val) => {
             setGenesisState(tabId, val);
           }}
-          value={getGenesisState(tabId) || ''}
+          value={genesisState}
+        />
+      </section>
+      <section>
+        <SubmitTx
+          {...submitExtrinsic}
+          valid={valid}
+          instanceId={instanceId}
+          chainId={chainId}
+          ss58Prefix={ss58}
+          units={units}
+          unit={unit}
+          existentialDeposit={existentialDeposit}
+          transactionVersion={String(transactionVersion)}
         />
       </section>
     </FormWrapper>
